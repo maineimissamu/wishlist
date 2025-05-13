0a7cc0e34f82c6cd13c2549e1e245133f3603cd2
Victor Vikhal <victor.vikhal@gmail.com>
2025-05-13 20:11:42 +0200

    REsponsividad dada a ambos componentes

--- src/Components/WishCard.jsx
+++ src/Components/WishCard.jsx
@@ -8,16 +8,26 @@ const {id} = useParams()
     
 
     return(
-       
-            <div className="bloque">
-            <li key={wish.id}>
-                <Link to={`/wisheslist/${id}/wish/${wish.id}`} className="bloque-link">
-                <h2>{wish.name}</h2>
-                <p>{wish.description}</p>
-                <p>Status: {wish.status}</p>
-                </Link>
-            </li>
+        <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg rounded-3xl overflow-hidden shadow-[0_10px_30px_rgba(0,0,0,0.5)] border border-gray-200 bg-white ">
+            <div className="relative h-40 w-full bg-cover bg-center"
+                 style={{backgroundImage: "url('https://img.freepik.com/foto-gratis/arreglo-coleccion-estacionaria-moderna_23-2149309643.jpg?semt=ais_hybrid&w=740')"}}>
             </div>
+
+            <div className="h-0.5 w-full bg-black"/>
+            <div key={wish.id} className="h-3/4 bg-gradient-to-b from-emerald-600 to-emerald-200 p-4 text-center text-sm  text-white ">
+            
+                <Link to={`/wisheslist/${id}/wish/${wish.id}`} className="block space-y-2">
+                <h3 className="font-semibold text-2xl md:text-3xl break-words text-balance ">{wish.name}</h3>
+                
+                <p className="text-black text-base">{wish.description}</p>
+                <p className="text-black text-base font-medium">Status: {wish.status}</p>
+                
+                </Link>
+            
+            </div>
+                
+        </div>
+           
       
        
     )
--- src/Components/WishesList.jsx
+++ src/Components/WishesList.jsx
@@ -38,15 +38,22 @@ export function WishesList () {
     }
     
         return(
-            <ul>
+            <div className="px-4 py-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
                 {wishes.map((wish) => {
                     return(
                         <WishCard wish={wish}/>
                     )
                 })}
-                <button onClick={goTo}>See all categories</button>
-            </ul>
+                
+            </div>
+            <div className="w-full flex justify-center mt-6">
+            <button onClick={goTo} className="flex items-center gap-2 bg-white text-green-800 font-bold py-2 px-6 rounded-md shadow-md hover:bg-gray-100 transition-all transform hover:scale-105">See all categories</button>
+            </div>    
+            
+            </div>    
         )
+
     
     
 }
